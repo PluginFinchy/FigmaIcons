61008e50fbc00ce04037d46ae480d2872e9738aa
Luke Finch <lukechristianfinch@gmail.com>
2021-01-28 22:37:49 +0000

    Icon Names

--- generate.js
+++ generate.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const sass = require('node-sass');
 const iconjson = require('./icons.json')
 const icons = Object.entries(iconjson)
-
+var sizedIcons = []
 
 if (!fs.existsSync('./dist')){
     fs.mkdirSync('./dist');
@@ -11,13 +11,23 @@ if (!fs.existsSync('./dist/icons')){
     fs.mkdirSync('./dist/icons');
 }
 
+icons.forEach(group => {    
+let name = group[0] != "medium" ? '--'+group[0] : '' ;
+
+Object.entries(group[1]).map(icon => {
+    icon[0] += name
+    sizedIcons.push([icon[0],icon[1]])
+})
 
 
-icons.forEach(icon => {
+})
+
+
+
+sizedIcons.forEach(icon => {
     let data = icon[1].replace('data:image/svg+xml;base64,', "");
 
     const contents = Buffer.from(data, 'base64');
-
 
     fs.writeFile(`./dist/icons/${icon[0]}.svg`,contents, 'utf8', (err) => {
         err ? console.log(err) : null;
@@ -25,8 +35,7 @@ icons.forEach(icon => {
 })
 
 
-
-const iconScss = '.icon{\n'+ icons.map(icon => {
+const iconScss = '.icon{\n'+ sizedIcons.map(icon => {
     return `&--${icon[0]} {\nbackground-image:url('./icons/${icon[0]}.svg')\n}`
 }).join('\n') + '\n}'
 
@@ -38,7 +47,7 @@ const iconScss = '.icon{\n'+ icons.map(icon => {
     }
 })
 
-const iconMd = `Icons exported from the UI2 Figma community file\nInstall via npm \`npm install @lukefinch/figmaicons\` \n \`import figmaicons from @lukefinch/figmaicons/dist/figmaicons.css\` \n\n|Icon|Name|\n|-|-|\n`+icons.map(icon => {
+const iconMd = `Icons exported from the UI2 Figma community file\nInstall via npm \`npm install @lukefinch/figmaicons\` \n \`import figmaicons from @lukefinch/figmaicons/dist/figmaicons.css\` \n\n|Icon|Name|\n|-|-|\n`+sizedIcons.map(icon => {
     return `|![${icon[0]}](dist/icons/${icon[0]}.svg)|icon--${icon[0]}`
 }).join('\n')
 
